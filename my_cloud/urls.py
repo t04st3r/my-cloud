005651b4144323183c0c485d03689c9b3cbdefc2
Raffaele Tosti <raffaele@digisin.it>
2019-01-15 16:13:41 +0100

    authentication with email address

--- my_cloud/urls.py
+++ my_cloud/urls.py
@@ -18,12 +18,14 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from .forms import EmailAuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('file_handler.urls')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html')),
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html',
+                                                authentication_form=EmailAuthenticationForm)),
     path('logout/', auth_views.LogoutView.as_view())
 ]
 
